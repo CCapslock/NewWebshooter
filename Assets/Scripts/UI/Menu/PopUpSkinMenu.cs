37048a2caeee45507c2396dd5714b452624575f7
DronCorleone <48313301+DronCorleone@users.noreply.github.com>
2021-05-18 17:41:23 +0300

    Commit 3

--- Assets/Scripts/UI/Menu/PopUpSkinMenu.cs
+++ Assets/Scripts/UI/Menu/PopUpSkinMenu.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopUpSkinMenu : BaseMenu
 {
@@ -13,16 +12,44 @@ public class PopUpSkinMenu : BaseMenu
     [Header("Shop Item")]
     [SerializeField] private ShopItem _shopItem;
 
+    [Header("Button hide")]
+    [SerializeField] private Button _btnHide;
+    
+
+    private void Start()
+    {
+        GameEvents.Current.OnUnlockGloves += ShowPopUp;
+        GameEvents.Current.OnUnlockWeb += ShowPopUp;
+
+        _btnHide.onClick.AddListener(() => Hide());
+    }
 
     public override void Hide()
     {
         _panel.SetActive(false);
-        _particles.Play();
+        //_particles.Stop();
     }
 
     public override void Show()
     {
         _panel.SetActive(true);
-        _particles.Stop();
+        //_particles.Play();
+    }
+
+    private void ShowPopUp(GlovesSkinModel skin)
+    {
+        _shopItem.SetPopUp(skin);
+        Show();
+    }
+    private void ShowPopUp(WebSkinModel skin)
+    {
+        _shopItem.SetPopUp(skin);
+        Show();
+    }
+
+    private void OnDestroy()
+    {
+        GameEvents.Current.OnUnlockGloves -= ShowPopUp;
+        GameEvents.Current.OnUnlockWeb -= ShowPopUp;
     }
 }
