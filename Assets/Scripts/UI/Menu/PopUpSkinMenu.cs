be57818a3d7b4d8374cd7070d61ed2ae801091aa
DronCorleone <48313301+DronCorleone@users.noreply.github.com>
2021-05-18 16:29:20 +0300

    Commit 2

--- Assets/Scripts/UI/Menu/PopUpSkinMenu.cs
+++ Assets/Scripts/UI/Menu/PopUpSkinMenu.cs
@@ -7,15 +7,22 @@ public class PopUpSkinMenu : BaseMenu
     [Header("Panel")]
     [SerializeField] private GameObject _panel;
 
+    [Header("Particles")]
+    [SerializeField] private ParticleSystem _particles;
+
+    [Header("Shop Item")]
+    [SerializeField] private ShopItem _shopItem;
 
 
     public override void Hide()
     {
         _panel.SetActive(false);
+        _particles.Play();
     }
 
     public override void Show()
     {
         _panel.SetActive(true);
+        _particles.Stop();
     }
 }
