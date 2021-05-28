5c3b666c01125b411905b45b9f93046aa76e6437
DronCorleone <48313301+DronCorleone@users.noreply.github.com>
2021-05-29 01:31:53 +0300

    Coin particles

--- Assets/Scripts/ThrowingObject.cs
+++ Assets/Scripts/ThrowingObject.cs
@@ -2,77 +2,81 @@
 
 public class ThrowingObject : MonoBehaviour
 {
-	public GameObject SpiderWeb;
-	public bool IsStucked;
-	public bool IsCoin;
+    public GameObject SpiderWeb;
+    public bool IsStucked;
+    public bool IsCoin;
 
-	[HideInInspector] public BonusLvlController bonusLvlController;
-	private Rigidbody _rigidbody;
-	private GameObject Web;
-	private Vector3 CustomWebPosition;
-	private Vector3 ThrowingVector;
-	private Vector3 RotationVector;
-	private float _throwingForce = 3000f;
-	private bool IsObjectStucked;
-	private bool IsObjectedWebed;
-	public bool NeedToRotate = true;
-	private void Awake()
-	{
-		IsStucked = false;
-		_rigidbody = GetComponent<Rigidbody>();
-		CustomWebPosition = new Vector3(0, 0, -0.3f);
+    [HideInInspector] public BonusLvlController bonusLvlController;
+    private Rigidbody _rigidbody;
+    private GameObject Web;
+    private Vector3 CustomWebPosition;
+    private Vector3 ThrowingVector;
+    private Vector3 RotationVector;
+    private float _throwingForce = 3000f;
+    private bool IsObjectStucked;
+    private bool IsObjectedWebed;
+    public bool NeedToRotate = true;
+    private void Awake()
+    {
+        IsStucked = false;
+        _rigidbody = GetComponent<Rigidbody>();
+        CustomWebPosition = new Vector3(0, 0, -0.3f);
 
-		int num1 = Random.Range(0, 3);
-		int num2 = Random.Range(0, 3 - num1);
-		int num3 = Random.Range(0, 3 - num1 - num2);
+        int num1 = Random.Range(0, 3);
+        int num2 = Random.Range(0, 3 - num1);
+        int num3 = Random.Range(0, 3 - num1 - num2);
 
-		RotationVector = new Vector3(num1, num2, num3);
-	}
-	private void FixedUpdate()
-	{
-		if (NeedToRotate)
-			RotateObject();
-	}
-	private void RotateObject()
-	{
-		_rigidbody.transform.Rotate(RotationVector);
-	}
-	private void OnCollisionEnter(Collision collision)
-	{
-		if (collision.gameObject.CompareTag("Wall") && !IsObjectStucked && IsObjectedWebed)
-		{
-			IsObjectStucked = true;
-			GetObjectStucked(collision);
-		}
-		if (collision.gameObject.CompareTag("Web"))
-		{
-			SphereCollider collider = collision.gameObject.GetComponent<SphereCollider>();
-			collider.isTrigger = true;
-			IsObjectedWebed = true;
-			ThrowingVector = transform.position;
-			ThrowingVector.z = 4500f;
-			ThrowingVector.x = (transform.position.x - collision.transform.position.x) * 1000;
-			ThrowingVector.y = (transform.position.y - collision.transform.position.y) * 2500;
-			_rigidbody.AddForce(ThrowingVector);
-		}
-	}
-	private void GetObjectStucked(Collision collision)
-	{
-		NeedToRotate = false;
-		gameObject.tag = "Wall";
-		_rigidbody.isKinematic = true;
-		IsStucked = true;
-		Web = Instantiate(SpiderWeb, transform.position + CustomWebPosition, Quaternion.identity);
-		Web.transform.Rotate(new Vector3(0, 180f, Random.Range(0, 360f)));
-		bonusLvlController.CaughtCoin();
-	}
-	public void TrowObjectUp()
-	{
-		_rigidbody.isKinematic = false;
-		_rigidbody.AddForce(Vector3.up * _throwingForce);
-	}
-	public void SetForce(float force)
-	{
-		_throwingForce = force;
-	}
+        RotationVector = new Vector3(num1, num2, num3);
+    }
+    private void FixedUpdate()
+    {
+        if (NeedToRotate)
+            RotateObject();
+    }
+    private void RotateObject()
+    {
+        _rigidbody.transform.Rotate(RotationVector);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall") && !IsObjectStucked && IsObjectedWebed)
+        {
+            IsObjectStucked = true;
+            GetObjectStucked(collision);
+        }
+        if (collision.gameObject.CompareTag("Web"))
+        {
+            SphereCollider collider = collision.gameObject.GetComponent<SphereCollider>();
+            collider.isTrigger = true;
+            IsObjectedWebed = true;
+            ThrowingVector = transform.position;
+            ThrowingVector.z = 4500f;
+            ThrowingVector.x = (transform.position.x - collision.transform.position.x) * 1000;
+            ThrowingVector.y = (transform.position.y - collision.transform.position.y) * 2500;
+            _rigidbody.AddForce(ThrowingVector);
+            if (IsCoin)
+            {
+                ParticlesController.Current.MakeStarsExplosion(collision.contacts[0].point);
+            }
+        }
+    }
+    private void GetObjectStucked(Collision collision)
+    {
+        NeedToRotate = false;
+        gameObject.tag = "Wall";
+        _rigidbody.isKinematic = true;
+        IsStucked = true;
+        Web = Instantiate(SpiderWeb, transform.position + CustomWebPosition, Quaternion.identity);
+        Web.transform.Rotate(new Vector3(0, 180f, Random.Range(0, 360f)));
+        bonusLvlController.CaughtCoin();
+    }
+    public void TrowObjectUp()
+    {
+        _rigidbody.isKinematic = false;
+        _rigidbody.AddForce(Vector3.up * _throwingForce);
+    }
+    public void SetForce(float force)
+    {
+        _throwingForce = force;
+    }
 }
